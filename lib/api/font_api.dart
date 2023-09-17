d2fcc796e9afd490f7b61b2e7009c5a461fabe64
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2023-09-17 14:11:16 +0530

    url changed

--- lib/api/font_api.dart
+++ lib/api/font_api.dart
@@ -5,7 +5,7 @@ import '../model/font_model.dart';
 class FontApi {
   static Future<FontModel> fetchFontList() async {
     var res = await get(Uri.parse(
-        "https://gitlab.com/Surajkrmkr/font-api/-/raw/main/fonts.json"));
+        "https://gitlab.com/piyushkpv/font-api/-/raw/main/fonts.json"));
 
     if (res.statusCode == 200) {
       return FontModel.fromJson(json.decode(res.body));
