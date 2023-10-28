16352e6f61f38d55b5cd6efc730754207e392cdb
Suraj Karmakar <53361745+Surajkrmkr@users.noreply.github.com>
2023-10-28 19:08:41 +0530

    directory enhanced

---
+++ lib/data/module/module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// class WindowBgLight extends StatelessWidget {
+//   const WindowBgLight({super.key});
+
+//   final pngName = "window_bg_light.9.png";
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // return ;
+//   }
+// }
