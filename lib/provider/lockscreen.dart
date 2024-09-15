53b74c6c393ec9cdb5ca3114bdf05c1cd99bd548
Piyushkpv <you@example.com>
2024-09-15 11:52:02 +0530

    changes for notifications

--- lib/provider/lockscreen.dart
+++ lib/provider/lockscreen.dart
@@ -54,71 +54,71 @@ class LockscreenProvider extends ChangeNotifier {
 
   void export({BuildContext? context}) async {
     setIsExporting = true;
-    final lockscreen = lockscreenXml.copy();
-    final themePath = CurrentTheme.getPath(context);
-    final eleProvider = Provider.of<ElementProvider>(context!, listen: false);
-    eleProvider.setActiveType = ElementType.swipeUpUnlock;
-    final elementList = eleProvider.elementList;
-    await Future.delayed(const Duration(seconds: 2), () {});
-    for (ElementWidget widget in elementList) {
-      final elementFromMap = elementWidgetMap[widget.type];
-      dynamic elementXmlFromMap;
-      // if (widget.type == ElementType.textLineClock) {
-      //   elementXmlFromMap = elementFromMap!["xml"]!(
-      //       textName: "d/E", color: "#ffffff", size: "60");
-      // } else {
-      //   elementXmlFromMap = elementFromMap!["xml"]!;
-      // }
-      if (elementFromMap!["isIconType"] ?? false) {
-        elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
-      } else if (elementFromMap["isMusic"] ?? false) {
-        elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
-        final previousText = lockscreen
-            .findAllElements("MusicControl")
-            .toList()
-            .firstWhere(
-                (element) => element.getAttribute("name") == "music_control")
-            .innerXml;
-        lockscreen
-            .findAllElements("MusicControl")
-            .toList()
-            .firstWhere(
-                (element) => element.getAttribute("name") == "music_control")
-            .innerXml = previousText + elementXmlFromMap;
-      } else if ((elementFromMap["isTextType"] ?? false) ||
-          (elementFromMap["isContainerType"] ?? false)) {
-        elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
-      } else {
-        elementXmlFromMap = elementFromMap["xml"]!;
-      }
-      if (!(elementFromMap["isMusic"] ?? false)) {
-        lockscreen
-            .findAllElements("Group")
-            .toList()
-            .firstWhere(
-                (element) => element.getAttribute("name") == widget.type!.name)
-            .innerXml = elementXmlFromMap;
-      }
-
-      if (elementFromMap["exportable"]) {
-        await Directory(platformBasedPath(
-                "$themePath\\lockscreen\\advance\\${elementFromMap["png"]["path"]}"))
-            .create(recursive: true)
-            .then((value) async {
-          await elementFromMap["png"]["export"](context);
-        });
-      }
+  final lockscreen = lockscreenXml.copy();
+  final themePath = CurrentTheme.getPath(context);
+  final eleProvider = Provider.of<ElementProvider>(context!, listen: false);
+  eleProvider.setActiveType = ElementType.swipeUpUnlock;
+  final elementList = eleProvider.elementList;
+  await Future.delayed(const Duration(seconds: 2), () {});
+  for (ElementWidget widget in elementList) {
+    final elementFromMap = elementWidgetMap[widget.type];
+    dynamic elementXmlFromMap;
+    // if (widget.type == ElementType.textLineClock) {
+    //   elementXmlFromMap = elementFromMap!["xml"]!(
+    //       textName: "d/E", color: "#ffffff", size: "60");
+    // } else {
+    //   elementXmlFromMap = elementFromMap!["xml"]!;
+    // }
+    if (elementFromMap!["isIconType"] ?? false) {
+      elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
+    } else if (elementFromMap["isMusic"] ?? false) {
+      elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
+      final previousText = lockscreen
+          .findAllElements("MusicControl")
+          .toList()
+          .firstWhere(
+              (element) => element.getAttribute("name") == "music_control")
+          .innerXml;
+      lockscreen
+          .findAllElements("MusicControl")
+          .toList()
+          .firstWhere(
+              (element) => element.getAttribute("name") == "music_control")
+          .innerXml = previousText + elementXmlFromMap;
+    } else if ((elementFromMap["isTextType"] ?? false) ||
+        (elementFromMap["isContainerType"] ?? false)) {
+      elementXmlFromMap = elementFromMap["xml"]!(ele: widget);
+    } else {
+      elementXmlFromMap = elementFromMap["xml"]!;
     }
-    await Future.delayed(const Duration(seconds: 15), () {});
-    lockscreen
-        .findAllElements("Group")
-        .toList()
-        .firstWhere((element) => element.getAttribute("name") == "bgAlpha")
-        .innerXml = getBgAlphaString(alpha: eleProvider.bgAlpha! * 255)!;
-    await File(
-            platformBasedPath("$themePath\\lockscreen\\advance\\manifest.xml"))
-        .writeAsString(lockscreen.toXmlString(pretty: true, indent: '\t'));
-    checkAlreadyExport(context: context);
+    if (!(elementFromMap["isMusic"] ?? false)) {
+      lockscreen
+          .findAllElements("Group")
+          .toList()
+          .firstWhere(
+              (element) => element.getAttribute("name") == widget.type!.name)
+          .innerXml = elementXmlFromMap;
+    }
+  
+    if (elementFromMap["exportable"]) {
+      await Directory(platformBasedPath(
+              "$themePath\\lockscreen\\advance\\${elementFromMap["png"]["path"]}"))
+          .create(recursive: true)
+          .then((value) async {
+        await elementFromMap["png"]["export"](context);
+      });
+    }
+  }
+  await Future.delayed(const Duration(seconds: 15), () {});
+  lockscreen
+      .findAllElements("Group")
+      .toList()
+      .firstWhere((element) => element.getAttribute("name") == "bgAlpha")
+      .innerXml = getBgAlphaString(alpha: eleProvider.bgAlpha! * 255)!;
+  await File(
+          platformBasedPath("$themePath\\lockscreen\\advance\\manifest.xml"))
+      .writeAsString(lockscreen.toXmlString(pretty: true, indent: '\t'));
+  checkAlreadyExport(context: context);
     setIsExporting = false;
   }
 
